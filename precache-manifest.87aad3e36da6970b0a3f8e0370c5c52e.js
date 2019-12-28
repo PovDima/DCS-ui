c55b497eb7dcf56dd2e0b90214f3996ea9cade56
Povetkin Dmitriy <povdima19@gmail.com>
2019-12-28 20:23:34 +0200

    Updates

---
+++ precache-manifest.87aad3e36da6970b0a3f8e0370c5c52e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4395c150fc5d6478d3becadf8fb98a83",
+    "url": "/DCS-ui/index.html"
+  },
+  {
+    "revision": "415233d428831f5c2e67",
+    "url": "/DCS-ui/static/css/main.efb4c113.chunk.css"
+  },
+  {
+    "revision": "d217d195fb3b70d9cdcf",
+    "url": "/DCS-ui/static/js/2.e570aa53.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/DCS-ui/static/js/2.e570aa53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "415233d428831f5c2e67",
+    "url": "/DCS-ui/static/js/main.ef4bd2ae.chunk.js"
+  },
+  {
+    "revision": "5fc5abfb104a3ed4b9b4",
+    "url": "/DCS-ui/static/js/runtime-main.3ffef9e2.js"
+  }
+]);
